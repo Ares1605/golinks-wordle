820e42584345cc77b6dcda806103c91996023dc0
Ares Stavropoulos <aresstav04@gmail.com>
2025-04-21 01:39:13 -0500

    remove wsgi compatibility, just use WSGI servers explicitly on app.py

--- backend/wsgi.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from dotenv import load_dotenv
-import os
-
-load_dotenv() # load environment variables from .env file
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
